8c0bb741b06f01c199127517e3ac6643f3bff6db
K750i <munkin80@gmail.com>
2023-03-13 12:47:07 +0800

    Generate another group of inputs with a button

--- script.js
+++ script.js
@@ -2,24 +2,63 @@
 
 let listGroupCount = 1;
 const group = document.querySelector(`#listGroup${listGroupCount}`);
-const moreSentenceBtn = document.querySelector(`.addsentence1`);
+const moreListBtn = document.querySelector('.addlist');
+const moreSentenceBtn = document.querySelector('.addsentence');
 
-const createSentenceInput = group => {
+const createListGroup = () => {
+  ++listGroupCount;
+  const fieldset = document.createElement('fieldset');
+  const legend = document.createElement('legend');
+  const mlabel = document.createElement('label');
+  const minput = document.createElement('input');
+  const slabel = document.createElement('label');
+  const sinput = document.createElement('input');
+  const mparagraph = document.createElement('p');
+  const sparagraph = document.createElement('p');
+
+  fieldset.setAttribute('id', `listGroup${listGroupCount}`);
+
+  mlabel.setAttribute('for', 'meaning1');
+  minput.setAttribute('id', 'meaning1');
+  minput.setAttribute('type', 'text');
+  mparagraph.appendChild(mlabel).textContent = 'Meaning 1';
+  mparagraph.appendChild(minput);
+
+  slabel.setAttribute('for', 'sentence1');
+  sinput.setAttribute('id', 'sentence1');
+  sinput.setAttribute('type', 'text');
+  sparagraph.appendChild(slabel).textContent = 'Sentence 1';
+  sparagraph.appendChild(sinput);
+
+  const button = document.createElement('button');
+  button.setAttribute('type', 'button');
+  button.setAttribute('class', 'addsentence');
+  button.addEventListener('click', e => createSentenceInput(e, fieldset));
+
+  fieldset.appendChild(legend).textContent = `List${listGroupCount}`;;
+  fieldset.appendChild(mparagraph);
+  fieldset.appendChild(sparagraph);
+  fieldset.appendChild(button).textContent = '⌄';
+
+  document.body.insertBefore(fieldset, moreListBtn.parentElement);
+};
+
+const createSentenceInput = (e, group) => {
   const count = group.querySelectorAll('input[type="text"]').length;
   const label = document.createElement('label');
   label.setAttribute('for', `sentence${count}`);
-  label.textContent = `Sentence ${count}`;
 
   const input = document.createElement('input');
   input.setAttribute('type', 'text');
   input.setAttribute('id', `sentence${count}`);
 
   const paragraph = document.createElement('p');
-  paragraph.appendChild(label);
+  paragraph.appendChild(label).textContent = `Sentence ${count}`;
   paragraph.appendChild(input);
 
-  const parent = moreSentenceBtn.parentElement;
-  parent.insertBefore(paragraph, moreSentenceBtn);
+  const parent = e.target.parentElement;
+  parent.insertBefore(paragraph, e.target);
 };
 
-moreSentenceBtn.addEventListener('click', () => createSentenceInput(group));
+moreSentenceBtn.addEventListener('click', e => createSentenceInput(e, group));
+moreListBtn.addEventListener('click', createListGroup);
